89082543765ebff1aeca71fd44af0b45bf672ae7
David Rojo <rojo@rojorevolution.com>
2021-01-11 20:27:18 -0600

    added fucntionality to append a new list item to the history section dynamically

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,8 @@
 
 
 $(document).ready(function () {
-
+    // ========================
+    // API Call Section
     var APIKey = "27c1cb710c54f9c6192d48c06bd4dc82";
     var city = "Austin"
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + APIKey;
@@ -11,6 +12,22 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response) {
         console.log(response)
+    })
+
+
+    // ========================
+    var $searchInput = $("#search-box");
+    var $searchBtn = $("#search-button");
+    var history = $("#history")
+
+
+    // ========================
+    //event listener for search button
+    $searchBtn.on("click", function (event) {
+        event.preventDefault();
+
+        var historyLI = /*html*/`<li class="list-group-item">${$searchInput.val()}</li>`
+        history.append(historyLI)
     })
 
 })
