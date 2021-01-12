a09054ec5d4e193e5ad3503183d68a553734def0
David Rojo <rojo@rojorevolution.com>
2021-01-11 21:21:34 -0600

    Got initial Info Div to dunamically appear but only if the city variable is prefilled, need to figure out why it wont work when I fill it from my click event

--- assets/app.js
+++ assets/app.js
@@ -1,33 +1,65 @@
 // View Activity 10 as a resource
 
+// ========================
+var $searchInput = $("#search-box");
+var $searchBtn = $("#search-button");
+var $history = $("#history");
 
-$(document).ready(function () {
-    // ========================
-    // API Call Section
-    var APIKey = "27c1cb710c54f9c6192d48c06bd4dc82";
-    var city = "Austin"
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + APIKey;
+var $city = ""
+
+// $(document).ready(function () {
+// ========================
+// API Call Section
+var APIKey = "27c1cb710c54f9c6192d48c06bd4dc82";
+// var city = ""
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + $city + "&appid=" + APIKey;
+
+function showResults() {
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        console.log(response);
+
+
+        var $city = response.name;
+        var $temperature = response.main.temp + "° F"
+        var $humidity = response.main.humidity;
+        var $wind = response.wind.speed + " MPH";
+        // var = uvIndex: //UV index requires different API? https://openweathermap.org/api/uvi
+
+
+        var resultsDiv = /*html*/`
+            <div class="card">
+                <div class="card-body p5">
+                    <h2 id="city-name" class="card-title mb-3">${$city}</h2>
+                    <p id="city-temp" class="card-text">Temperature: ${$temperature}</p>
+                    <p id="city-humidity" class="card-text">Humidity: ${$humidity} </p>
+                    <p id="city-wind" class="card-text">Wind Speed: ${$wind}</p>
+                    <p id="city-uv" class="card-text">UV Index: <span
+                        class="badge bg-danger text-light">9.49</span></p>
+                </div>
+            </div>`
+
+        $("#city-info").append(resultsDiv)
+
+
     })
+}
+
+// ========================
+//event listener for search button
 
 
-    // ========================
-    var $searchInput = $("#search-box");
-    var $searchBtn = $("#search-button");
-    var history = $("#history")
+// })
 
+$searchBtn.on("click", function (event) {
+    event.preventDefault();
+    $city = $searchInput.val().trim();
+    var historyLI = /*html*/`<li id="${$searchInput.val()}"class="list-group-item">${$searchInput.val()}</li>`
+    $history.append(historyLI)
 
-    // ========================
-    //event listener for search button
-    $searchBtn.on("click", function (event) {
-        event.preventDefault();
-
-        var historyLI = /*html*/`<li class="list-group-item">${$searchInput.val()}</li>`
-        history.append(historyLI)
-    })
-
+    // showResults()
 })
+
+
