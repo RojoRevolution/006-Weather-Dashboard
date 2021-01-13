ca506f7291e0dd144dba404857b7f92704202315
David Rojo <rojo@rojorevolution.com>
2021-01-12 22:01:14 -0600

    added explanatory comments to JS file, lest I forget what I'm doing

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
-
-    // View Activity 10 as a resource
-
     // ========================
     var $searchInput = $("#search-box");
     var $searchBtn = $("#search-button");
     var $history = $("#history");
 
 
-    // Need a variable that passes the $searchInput.val() from the click event
+    // Variable holds the current city that is being searched
     var $city = "";
 
     // ========================
-    // API Call Section
+    // API Key
     var APIKey = "27c1cb710c54f9c6192d48c06bd4dc82";
 
+    //=================================
+    //Calls the Five Day Forcast Response from API
     function renderConditions() {
         var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + $city + "&appid=" + APIKey;
 
@@ -22,12 +21,18 @@ $(document).ready(function () {
             url: weatherQueryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
+            // console.log(response);
+
+            // Vars grab the needed responses from API
             var $city = response.name;
             var $temperature = response.main.temp + "° F"
             var $humidity = response.main.humidity;
             var $wind = response.wind.speed + " MPH";
+
+            // Need to figure out the UV index
             // var = uvIndex: //UV index requires different API? https://openweathermap.org/api/uvi
+
+            //Renders Content to the page
             var resultsDiv = /*html*/`
             <div class="card">
                 <div class="card-body p5">
@@ -39,12 +44,14 @@ $(document).ready(function () {
                         class="badge bg-danger text-light">9.49</span></p>
                 </div>
             </div>`
+            //Empty clears the div before appending so content does not stack infinitely
             $("#city-info").empty(resultsDiv)
             $("#city-info").append(resultsDiv)
         })
     }
 
-    //call Forcast
+    //=================================
+    //Calls the Five Day Forcast Response from API
     function renderForcast() {
         var forcastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + $city + "&cnt=40&appid=" + APIKey;
 
@@ -52,35 +59,44 @@ $(document).ready(function () {
             url: forcastQueryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            // console.log(response)
 
+            // Adds H3 and the Row div before running rest of content through a loop
             var forcastContainers = /*html*/`
-          <h3>Five Day Forcast</h3>
+            <h3>Five Day Forcast</h3>
             <div class="row forcastRow"></div>`
+            //Empty clears div of content before rerendering
             $(".forcast").empty(forcastContainers)
             $(".forcast").append(forcastContainers)
 
-
+            // Calls the render function
             renderFiveDays(response)
         })
     }
 
+    //=================================
+    // Renders the 5 Day forecast cards using a loop,
     function renderFiveDays(response) {
         for (var i = 7; i <= 40; i += 8) {
-            //date
+            //Grabs the date response
             var $responseDate = response.list[i].dt_txt;
+            //Removes the time from the date response
             var $rmvHour = $responseDate.substr(0, 10);
+            //Removes the year from the front of the response, the adds -21 at the end -- this is not good code for the long run but it works for now with the API I am using
             var $date = $rmvHour.substr(5, 9) + "-21"
-            console.log($date)
-            //Icon
+
+            // ----- NEED TO FIGURE OUT ICON
             // var $responseIcon = response.list[8].weather
-            //max temp
+
+            //Grabs the maximum temp from the 3:00pm time slot - I did not find a free API that gave me the max temp
             var $responseMaxTemp = response.list[i].main.temp_max
+            //Rounds the max temp so we do not display a decimal
             var $max_temp = Math.round($responseMaxTemp)
-            console.log($max_temp)
-            //humidity
+            // console.log($max_temp)
+
+            //Grabs the humidity response
             var $responseHumid = response.list[i].main.humidity
-            console.log($responseHumid)
+            // console.log($responseHumid)
 
             var forcastContent = /*html*/`
                 <div class="col-lg-2">
@@ -99,9 +115,6 @@ $(document).ready(function () {
 
     }
 
-
-
-
     // ========================
     //event listener for search button
     $searchBtn.on("click", function (event) {
@@ -115,7 +128,7 @@ $(document).ready(function () {
         renderForcast()
     })
 
-    // Event clicker for history buttons to rerender info ---- Unfinished
+    // Event listener for history buttons to rerender info 
     $("#history").on("click", "li", function (event) {
         event.preventDefault();
         $city = $(this).text().trim();
